6d4108eaf4dbe92eea4ca38b9f75ed2b4d21a3f5
Chengetai Chitemerere <CHTCHE010@myuct.ac.za>
2023-08-02 18:08:09 +0200

    Same changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
   { path: 'initialsignup', component: InitialsignupComponent},
   { path: 'login', component: LoginComponent},
   { path: 'adminprofile', component: AdminprofileComponent},
-  { path: 'caprofile', component: CaProfileComponent},
+  { path: 'caprofile', component: CaProfileComponent}
 ];
 
 @NgModule({
