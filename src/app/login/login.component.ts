4d48d70161a5fe31ce74f3458d66502a28f3f72b
Meg Nicholls <nchmeg001@myuct.ac.za>
2023-08-03 21:18:49 +0200

    // Login and signup testing

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,6 +6,7 @@ import { SharedService } from '../shared.service';
 import { Router } from '@angular/router';
 import { UserStoreService } from '../services/user-store.service';
 import { AuthService } from '../services/auth.service';
+import { initialsignup } from '../models/initialsignup.model';
 
 @Component({
   selector: 'app-login',
@@ -15,6 +16,7 @@ import { AuthService } from '../services/auth.service';
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
   passwordVisible: boolean = true;
+  InitialSignUp = new initialsignup();
 
   constructor(
     private formBuilder: FormBuilder,
@@ -24,6 +26,12 @@ export class LoginComponent implements OnInit {
     private router: Router, 
     private authService: AuthService
     ) {}
+
+  login(InitialSignUp: initialsignup) {
+    this.authService.login(InitialSignUp).subscribe((token: string) => {
+      localStorage.setItem('authToken', token);
+    });
+  }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
@@ -36,59 +44,63 @@ export class LoginComponent implements OnInit {
     this.passwordVisible = !this.passwordVisible;
   }
 
-// Method to handle form submission
-
-async handleSubmit() {
-  // Prepare the login data
-  const loginObj = {
-    // Add your login data properties here
-    username: 'your-username',
-    password: 'your-password'
-  };
-
-  try {
-    // Call the login method in the SharedService with the login data
-    const response = await this.sharedService.login(loginObj).toPromise(); // Convert observable to promise
-
-    const token = response.token;
-    // Store the token in local storage
-    localStorage.setItem('token', token);
-    // Set the user payload in the shared service
-    this.sharedService.setUserPayload(token);
-    // Redirect to the appropriate page
-    const accountUserId = this.sharedService.getAccountUserIdFromToken();
-    const accountType = this.sharedService.getaccountTypeFromToken();
-    if (accountUserId) {
-      // User is logged in and has an account ID
-      if (accountType === 'administrator') {
-        this.router.navigate(['/adminprofile']);
-      } else if (accountType === 'serviceprovider') {
-        this.router.navigate(['/serviceproviderprofile']);
-      } else if (accountType === 'ClaimsAgent') {
-        this.router.navigate(['/claimsagentprofile']);
-      } else {
-        // Handle the case where accountType is neither administrator, serviceprovider, nor ClaimsAgent
-        // You can decide where to redirect them in this case (e.g., back to the login page)
-        this.router.navigate(['/login']);
-      }
-    } else {
-      // User is logged in but does not have an account ID
-      if (accountType === 'ClaimsAgent') {
-        this.router.navigate(['/casignup']);
-      } else if (accountType === 'serviceprovider') {
-        this.router.navigate(['/spsignup']);
-      } else {
-        // Handle the case where accountType is neither ClaimsAgent nor serviceprovider
-        // You can decide where to redirect them in this case (e.g., back to the login page)
-        this.router.navigate(['/login']);
-      }
-    }
-  } catch (error) {
-    // Handle error if login fails (e.g., show an error message to the user)
-    console.error('Login error:', error);
-    // You can also redirect to an error page or show an error message to the user
+  handleSubmit(){
+    this.login(this.InitialSignUp);
   }
-}
+
+// async handleSubmit() {
+//   // Prepare the login data
+//   const loginObj = {
+//     // Add your login data properties here
+//     username: 'your-username',
+//     password: 'your-password'
+//   };
+
+//   try {
+//     // Call the login method in the SharedService with the login data
+//     const response = await this.sharedService.login(loginObj).toPromise(); // Convert observable to promise
+
+//     const token = response.token;
+//     // Store the token in local storage
+//     localStorage.setItem('token', token);
+//     // Set the user payload in the shared service
+//     this.sharedService.setUserPayload(token);
+//     // Redirect to the appropriate page
+//     const accountUserId = this.sharedService.getAccountUserIdFromToken();
+//     const accountType = this.sharedService.getaccountTypeFromToken();
+//     if (accountUserId) {
+//       // User is logged in and has an account ID
+//       if (accountType === 'administrator') {
+//         this.router.navigate(['/adminprofile']);
+//       } else if (accountType === 'serviceprovider') {
+//         this.router.navigate(['/serviceproviderprofile']);
+//       } else if (accountType === 'ClaimsAgent') {
+//         this.router.navigate(['/claimsagentprofile']);
+//       } else {
+//         // Handle the case where accountType is neither administrator, serviceprovider, nor ClaimsAgent
+//         // You can decide where to redirect them in this case (e.g., back to the login page)
+//         this.router.navigate(['/login']);
+//       }
+//     } else {
+//       // User is logged in but does not have an account ID
+//       if (accountType === 'ClaimsAgent') {
+//         this.router.navigate(['/casignup']);
+//       } else if (accountType === 'serviceprovider') {
+//         this.router.navigate(['/spsignup']);
+//       } else {
+//         // Handle the case where accountType is neither ClaimsAgent nor serviceprovider
+//         // You can decide where to redirect them in this case (e.g., back to the login page)
+//         this.router.navigate(['/login']);
+//       }
+//     }
+//   } catch (error) {
+//     // Handle error if login fails (e.g., show an error message to the user)
+//     console.error('Login error:', error);
+//     // You can also redirect to an error page or show an error message to the user
+//   }
+// }
+
+
 }
 
 
--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -11,195 +11,169 @@ import { addserviceproviderbank } from './models/addserviceproviderbank';
 import { addserviceprovideraddress } from './models/addserviceprovideraddress';
 
 @Injectable({
-
   providedIn: 'root'
-
 })
 
 export class SharedService {
-    baseAPIUrl = "http://localhost:5184/";
-    private userPayload: any;
-    private jwtHelper: JwtHelperService;
+    // baseAPIUrl = "http://localhost:5184/";
+    // private userPayload: any;
+    // private jwtHelper: JwtHelperService;
 
-    constructor(
-        private http: HttpClient,
-        private router: Router) {
-            const token = this.getToken();
-            this.userPayload = this.decodeToken(token);
-            this.jwtHelper = new JwtHelperService();
-         }
+    // constructor(
+    //     private http: HttpClient,
+    //     private router: Router) {
+    //         const token = this.getToken();
+    //         this.userPayload = this.decodeToken(token);
+    //         this.jwtHelper = new JwtHelperService();
+    //      }
 
  
 
-    // Claims agent
+    // // Claims agent
 
-    addClaim(addClaimsRequest : addclaim): Observable<addclaim> {    
-        return this.http.post<addclaim>(this.baseAPIUrl + 'api/Claims/Add-Claim', addClaimsRequest);
-    }
+    // addClaim(addClaimsRequest : addclaim): Observable<addclaim> {    
+    //     return this.http.post<addclaim>(this.baseAPIUrl + 'api/Claims/Add-Claim', addClaimsRequest);
+    // }
 
-    addClaimsAgent(addClaimsAgentRequest : addclaimsagent): Observable<addclaimsagent> {    
-        return this.http.post<addclaimsagent>(this.baseAPIUrl + 'api/ClaimsAgent/Create Claims Agent Profile', addClaimsAgentRequest);
-    }
+    // addClaimsAgent(addClaimsAgentRequest : addclaimsagent): Observable<addclaimsagent> {    
+    //     return this.http.post<addclaimsagent>(this.baseAPIUrl + 'api/ClaimsAgent/Create Claims Agent Profile', addClaimsAgentRequest);
+    // }
 
-    // Service Provider
-    addServiceProviderDetails(addServiceProviderDetailsRequest : addserviceproviderdetails): Observable<addserviceproviderdetails> {    
-        return this.http.post<addserviceproviderdetails>(this.baseAPIUrl + 'api/ServiceProvider/Create-ServiceProvider-Profile', addServiceProviderDetailsRequest);
-    }
+    // // Service Provider
+    // addServiceProviderDetails(addServiceProviderDetailsRequest : addserviceproviderdetails): Observable<addserviceproviderdetails> {    
+    //     return this.http.post<addserviceproviderdetails>(this.baseAPIUrl + 'api/ServiceProvider/Create-ServiceProvider-Profile', addServiceProviderDetailsRequest);
+    // }
 
-    addServiceProviderAddress(addServiceProviderAddressRequest : addserviceproviderbank): Observable<addserviceprovideraddress> {
-        return this.http.post<addserviceprovideraddress>(this.baseAPIUrl + 'api/ServiceProvider/Add-Address', addServiceProviderAddressRequest);
-    }
+    // addServiceProviderAddress(addServiceProviderAddressRequest : addserviceproviderbank): Observable<addserviceprovideraddress> {
+    //     return this.http.post<addserviceprovideraddress>(this.baseAPIUrl + 'api/ServiceProvider/Add-Address', addServiceProviderAddressRequest);
+    // }
 
-    addServiceProviderBank(addServiceProviderBankRequest: addserviceproviderbank): Observable<addserviceproviderbank> {
-        return this.http.post<addserviceproviderbank>(this.baseAPIUrl + 'api/ServiceProvider/Add-Bank-Details', addServiceProviderBankRequest);
-    }
+    // addServiceProviderBank(addServiceProviderBankRequest: addserviceproviderbank): Observable<addserviceproviderbank> {
+    //     return this.http.post<addserviceproviderbank>(this.baseAPIUrl + 'api/ServiceProvider/Add-Bank-Details', addServiceProviderBankRequest);
+    // }
 
-    getServiceProviderProfile(): Observable<any> {
-        return this.http.get<any>(`${this.baseAPIUrl}/api/Get-Profile`);
-      }
+    // getServiceProviderProfile(): Observable<any> {
+    //     return this.http.get<any>(`${this.baseAPIUrl}/api/Get-Profile`);
+    //   }
 
-    getServiceProviderAddress(AddressId: string): Observable<any> {
-        return this.http.get<any>(`${this.baseAPIUrl}/api/Get-Address-by-Id`);
-      }
+    // getServiceProviderAddress(AddressId: string): Observable<any> {
+    //     return this.http.get<any>(`${this.baseAPIUrl}/api/Get-Address-by-Id`);
+    //   }
 
-    getServiceProviderBank(BankDetailsId: string): Observable<any> {
-         return this.http.get<any>(`${this.baseAPIUrl}/api/Get-Bank-Details-by-Id`);
-      }
+    // getServiceProviderBank(BankDetailsId: string): Observable<any> {
+    //      return this.http.get<any>(`${this.baseAPIUrl}/api/Get-Bank-Details-by-Id`);
+    //   }
 
-    // All
-    addInitialSignup(initialSignupRequest : initialsignup): Observable<initialsignup> {    
-        return this.http.post<initialsignup>(this.baseAPIUrl + 'Auth/Register', initialSignupRequest);
-       }
+    // // All
+    // addInitialSignup(initialSignupRequest : initialsignup): Observable<initialsignup> {    
+    //     return this.http.post<initialsignup>(this.baseAPIUrl + 'Auth/Register', initialSignupRequest);
+    //    }
 
-    login(loginObj: any) {
-        return this.http.post<any>(this.baseAPIUrl + 'Auth/Login', loginObj)
-    }
+    // // public login(initialsignup: User): Observable<string> {
+    // //     return this.http.post(this.baseAPIUrl + 'Auth/Login', initialsignup, {
+    // //         responseType: 'text',
+    // //     });
+    // // }
 
-    isProfileExists(Account_UserId: string): Observable<boolean> {
-        return this.http.get<boolean>(this.baseAPIUrl + 'Auth/CheckUserProfile' + Account_UserId);
-    }
+    // login(loginObj: any) {
+    //     return this.http.post<any>(this.baseAPIUrl + 'Auth/Login', loginObj)
+    // }
+
+    // isProfileExists(Account_UserId: string): Observable<boolean> {
+    //     return this.http.get<boolean>(this.baseAPIUrl + 'Auth/CheckUserProfile' + Account_UserId);
+    // }
     
-    signOut(){
-
-        localStorage.clear();
-
-        this.router.navigate(['login'])
-
-    }
-
- 
-
-    storeToken(tokenValue: string){
-
-        localStorage.setItem('token', tokenValue)
-
-    }
-
- 
-
-    getToken() {
-
-        return localStorage.getItem('token')
-
-    }
-
- 
-
-    isLoggedIn(): boolean {
-
-        return !!localStorage.getItem('token')
-
-    }
-
-    decodeToken(token: string | null) {
-        if (token) {
-            try {
-                const decodedToken = this.jwtHelper.decodeToken(token);
-                console.log('Decoded Token:', decodedToken);
-                return decodedToken;
-            } catch (error) {
-                console.error('Error decoding token:', error);
-                return null; // Handle the error gracefully
-            }
-        } else {
-            console.warn('No token found.');
-            return null; // Handle the case where no token is found
-        }
-    }
-
-    setUserPayload(token: string) {
-        this.userPayload = this.jwtHelper.decodeToken(token);
-    }
-
-    getusernameFromToken(){
-
-        if(this.userPayload)
-
-        return this.userPayload.username;
-
-    }
-
- 
-
-    getemailFromToken(){
-
-        if(this.userPayload)
-
-        return this.userPayload.email;
-
-    }
-
- 
-
-    getaccountTypeFromToken(){
-
-        if(this.userPayload)
-
-        return this.userPayload.accountType;
-
-    }
-
- 
-
-    getAccountUserIdFromToken(){
-        if(this.userPayload)
-        return this.userPayload.Account_UserId;
-    }
-
-    // updateStudent(val: any) {
-
-    //     return this.http.put(this.APIUrl + '/Student', val);
-
+    // signOut(){
+    //     localStorage.clear();
+    //     this.router.navigate(['login'])
     // }
 
-    // deleteStudent(id: any) {
-
-    //     return this.http.delete(this.APIUrl + '/Student/' + id);
-
+    // storeToken(tokenValue: string){
+    //     localStorage.setItem('token', tokenValue)
     // }
 
-    // getDepartmentList(): Observable < any[] > {
-
-    //     return this.http.get < any > (this.APIUrl + '/Department');
-
+    // getToken() {
+    //     return localStorage.getItem('token')
     // }
 
-    // addDepartment(val: any) {
-
-    //     return this.http.post(this.APIUrl + '/Department', val);
-
+    // isLoggedIn(): boolean {
+    //     return !!localStorage.getItem('token')
     // }
 
-    // updateDepartment(val: any) {
-
-    //     return this.http.put(this.APIUrl + '/Department', val);
-
+    // decodeToken(token: string | null) {
+    //     if (token) {
+    //         try {
+    //             const decodedToken = this.jwtHelper.decodeToken(token);
+    //             console.log('Decoded Token:', decodedToken);
+    //             return decodedToken;
+    //         } catch (error) {
+    //             console.error('Error decoding token:', error);
+    //             return null; // Handle the error gracefully
+    //         }
+    //     } else {
+    //         console.warn('No token found.');
+    //         return null; // Handle the case where no token is found
+    //     }
     // }
 
-    // deleteDepartment(id: any) {
-
-    //     return this.http.delete(this.APIUrl + '/Department/' + id);
-
+    // setUserPayload(token: string) {
+    //     this.userPayload = this.jwtHelper.decodeToken(token);
     // }
+
+    // getusernameFromToken(){
+    //     if(this.userPayload)
+    //     return this.userPayload.username;
+    // }
+
+    // getemailFromToken(){
+    //     if(this.userPayload)
+    //     return this.userPayload.email;
+    // }
+
+    // getaccountTypeFromToken(){
+    //     if(this.userPayload)
+    //     return this.userPayload.accountType;
+    // }
+
+    // getAccountUserIdFromToken(){
+    //     if(this.userPayload)
+    //     return this.userPayload.Account_UserId;
+    // }
+
+    // // updateStudent(val: any) {
+
+    // //     return this.http.put(this.APIUrl + '/Student', val);
+
+    // // }
+
+    // // deleteStudent(id: any) {
+
+    // //     return this.http.delete(this.APIUrl + '/Student/' + id);
+
+    // // }
+
+    // // getDepartmentList(): Observable < any[] > {
+
+    // //     return this.http.get < any > (this.APIUrl + '/Department');
+
+    // // }
+
+    // // addDepartment(val: any) {
+
+    // //     return this.http.post(this.APIUrl + '/Department', val);
+
+    // // }
+
+    // // updateDepartment(val: any) {
+
+    // //     return this.http.put(this.APIUrl + '/Department', val);
+
+    // // }
+
+    // // deleteDepartment(id: any) {
+
+    // //     return this.http.delete(this.APIUrl + '/Department/' + id);
+
+    // // }
 
 }
